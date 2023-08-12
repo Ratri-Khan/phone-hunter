7f42401d99210721c5afd43edf5ec0f84cb7ee7e
ratrikhanam000 <ratrikhanam000@gmail.com>
2023-08-12 12:43:53 +0600

    mobile details showing is start

--- app.js
+++ app.js
@@ -1,26 +1,74 @@
 
-const loadPhones = () =>{
-    fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
-    .then(res =>res.json())
+
+const loadPhones = (searchText) =>{
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+    .then(res => res.json())
     .then(data => displayPhones(data.data))
 }
-
 const displayPhones = (phones) =>{
-    console.log(phones);
-phones.forEach(phone =>{
+    console.log(phones)
     const phonesContainer = document.getElementById('phones-container');
-    const phoneDiv  = document.createElement('div');
-    // phoneDiv.classList.add('col');
-    phoneDiv.innerHTML = `
-    <div class="border-2 p-5 border-fuchsia-950 bg-fuchsia-200">
-        <img src="${phone.image}">
-        <h5 class="card-title">${phone.phone_name}</h5>           
-        <button>Show Details</button>    
-    </div>
-    `;
-    phonesContainer.appendChild(phoneDiv);
-});
-    
-} 
+    phonesContainer.textContent = '';
+    // show all button
+    const showAll = document.getElementById('show-all');
+    if(phones.length > 10){
+        phones = phones.slice(0,10);       
+        showAll.classList.remove('hidden');
+    }
+    else{
+        showAll.classList.add('hidden'); 
+    }
 
-loadPhones();
+
+    // not found massage
+    const notFound = document.getElementById('not-found');
+    if(phones.length === 0){
+        notFound.classList.remove('hidden');
+    }
+    else{
+        notFound.classList.add('hidden');
+    }
+    phones.map(phone =>{
+       
+        const phoneDiv = document.createElement('div');
+        phoneDiv.innerHTML=`
+          <div class="border-2 p-5 border-fuchsia-950 bg-fuchsia-200">
+             <img src="${phone.image}"/>
+             <h3>${phone.phone_name} </h3>
+             <p>${phone.brand}<p>
+             <button onclick="ShowMoreDetails('${phone.slug}')">Details</button>
+          </div>
+        `;
+        phonesContainer.appendChild(phoneDiv);
+        
+    })
+    taggleSpinner(false);
+}
+
+document.getElementById('search-button').addEventListener('click',function(){
+    taggleSpinner(true);
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhones(searchText);
+    
+
+})
+//for enter key 
+// document.getElementById('search-field').addEventListener('keypress',function(e){
+//     if(e.key === 'Enter'){
+//         processSearch(10);
+//     }
+// })
+//for spinner
+const taggleSpinner = loader =>{
+    const loaderSection = document.getElementById('loader-section');
+    if(loader){
+        loaderSection.classList.remove('hidden');
+    }
+    else{
+        loaderSection.classList.add('hidden');
+    }
+
+
+}
+// loadPhones();
