b6afe7c277a1441d7fee907c4804b79afb101cc4
ratrikhanam000 <ratrikhanam000@gmail.com>
2023-09-15 13:17:00 +0600

    all setup complete

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
-const loadPhones = (searchText) =>{
+const loadPhones = (searchText ,dataLimit) => {
     fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
-    .then(res => res.json())
-    .then(data => displayPhones(data.data))
+        .then(res => res.json())
+        .then(data => displayPhones(data.data, dataLimit))
 }
-const displayPhones = (phones) =>{
+const displayPhones = (phones ,dataLimit) => {
     // console.log(phones)
     const phonesContainer = document.getElementById('phones-container');
     phonesContainer.textContent = '';
-    // show all button
-    // const showAll = document.getElementById('show-all');
-    // if(phones.length > 10){
-    //     phones = phones.slice(0,10);       
-    //     showAll.classList.remove('hidden');
-    // }
-    // else{
-    //     showAll.classList.add('hidden'); 
-    // }
+    const showAll = document.getElementById('show-all');
+    // show all button    
+    if (dataLimit && phones.length > 10) {
+        phones = phones.slice(0, 9);
+        showAll.classList.remove('d-none');
+    }
+    else {
+        showAll.classList.add('d-none');
+    }
     // not found massage
     const notFound = document.getElementById('not-found');
-    if(phones.length === 0){
+    if (phones.length === 0) {
         notFound.classList.remove('d-none');
     }
-    else{
+    else {
         notFound.classList.add('d-none');
     }
-    phones.map(phone =>{
-       
+    phones.map(phone => {
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col')
-        phoneDiv.innerHTML=`        
+        phoneDiv.innerHTML = `        
           <div class="card p-0 m-3 border border-4 border-primary">
             <img src="${phone.image}" class="card-img-top p-4" alt="...">
             <div class="card-body text-center">
@@ -39,40 +38,52 @@ const displayPhones = (phones) =>{
         </div>
         `;
         phonesContainer.appendChild(phoneDiv);
-        
-    })
-    taggleSpinner(false);
-}
 
-document.getElementById('search-button').addEventListener('click',function(){
-    taggleSpinner(true);
+    })
+    toggleSpinner(false);
+}
+const processSearch = (dataLimit) =>{
+    toggleSpinner(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    loadPhones(searchText);
-    
-
+    loadPhones(searchText,dataLimit);
+}
+//for searching process
+document.getElementById('search-button').addEventListener('click', function () {
+processSearch(10);
 })
-const taggleSpinner = loader =>{
+
+// show all
+document.getElementById('btn-show-all').addEventListener('click',function(){
+  processSearch();
+})
+// enter key handler
+document.getElementById('search-field').addEventListener('keypress',function(e){
+    if(e.key === 'Enter'){
+        processSearch(10);
+    }
+})
+//for spinner
+const toggleSpinner = loader => {
     const loaderSection = document.getElementById('loader-section');
-    if(loader){
+    if (loader) {
         loaderSection.classList.remove('d-none');
     }
-    else{
+    else {
         loaderSection.classList.add('d-none');
     }
 }
-
 //show Details 
-const ShowMoreDetails = (id) =>{
+const ShowMoreDetails = (id) => {
     fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
-    .then(res=>res.json())
-    .then(data =>displayDetails(data.data))
+        .then(res => res.json())
+        .then(data => displayDetails(data.data))
 }
-const displayDetails = (phone) =>{
+const displayDetails = (phone) => {
     console.log(phone);
     const phoneDetailsMidalLabel = document.getElementById('phoneDetailsMidalLabel');
-   const test =  phoneDetailsMidalLabel.innerText = phone.name;
-   console.log(test);
+    const test = phoneDetailsMidalLabel.innerText = phone.name;
+    console.log(test);
     const modalBody = document.getElementById('modal-body');
     modalBody.innerHTML = `
       <div>
@@ -81,10 +92,9 @@ const displayDetails = (phone) =>{
         <p>Storage : ${phone.mainFeatures.storage}</p>
         <p>Memory: ${phone.mainFeatures.memory}</p>
         <p>Slug : ${phone.slug}</p>
-        <p>WLAN : ${phone.others.WLAN}</p>
+        <p>Wlan : ${phone.others.WLAN}</p>
         <p>Bluetooth : ${phone.others.Bluetooth}</p>
         <p>ReleaseDate : ${phone.releaseDate}</p>
       </div>
     `
-    // phoneDetailsMidalLabel.appendChild(modalBody);
 }
