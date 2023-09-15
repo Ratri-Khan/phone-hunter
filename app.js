48bf0b4b7017f6848d63288c35e0167d47feda9e
ratrikhanam000 <ratrikhanam000@gmail.com>
2023-09-15 10:18:30 +0600

    data showing complete

--- app.js
+++ app.js
@@ -1,43 +1,42 @@
-
-
 const loadPhones = (searchText) =>{
     fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     .then(res => res.json())
     .then(data => displayPhones(data.data))
 }
 const displayPhones = (phones) =>{
-    console.log(phones)
+    // console.log(phones)
     const phonesContainer = document.getElementById('phones-container');
     phonesContainer.textContent = '';
     // show all button
-    const showAll = document.getElementById('show-all');
-    if(phones.length > 10){
-        phones = phones.slice(0,10);       
-        showAll.classList.remove('hidden');
-    }
-    else{
-        showAll.classList.add('hidden'); 
-    }
-
-
+    // const showAll = document.getElementById('show-all');
+    // if(phones.length > 10){
+    //     phones = phones.slice(0,10);       
+    //     showAll.classList.remove('hidden');
+    // }
+    // else{
+    //     showAll.classList.add('hidden'); 
+    // }
     // not found massage
     const notFound = document.getElementById('not-found');
     if(phones.length === 0){
-        notFound.classList.remove('hidden');
+        notFound.classList.remove('d-none');
     }
     else{
-        notFound.classList.add('hidden');
+        notFound.classList.add('d-none');
     }
     phones.map(phone =>{
        
         const phoneDiv = document.createElement('div');
-        phoneDiv.innerHTML=`
-          <div class="border-2 p-5 border-fuchsia-950 bg-fuchsia-200">
-             <img src="${phone.image}"/>
-             <h3>${phone.phone_name} </h3>
-             <p>${phone.brand}<p>
-                <button onclick="ShowMoreDetails('${phone.slug}')" class="bg-fuchsia-950 text-white px-7 py-2 rounded-md" data-bs-toggle="modal" data-bs-target="#phoneDetailsMidal">Details</button>
-          </div>
+        phoneDiv.classList.add('col')
+        phoneDiv.innerHTML=`        
+          <div class="card p-0 m-3 border border-4 border-primary">
+            <img src="${phone.image}" class="card-img-top p-4" alt="...">
+            <div class="card-body text-center">
+              <h3 class='p-0'>${phone.phone_name} </h3>
+              <p>${phone.brand}<p>
+              <button onClick="ShowMoreDetails('${phone.slug}')" type="button" class="btn bg-dark text-white" data-bs-toggle="modal" data-bs-target="#exampleModal">Details</button>            
+           </div>
+        </div>
         `;
         phonesContainer.appendChild(phoneDiv);
         
@@ -53,20 +52,13 @@ document.getElementById('search-button').addEventListener('click',function(){
     
 
 })
-//for enter key 
-// document.getElementById('search-field').addEventListener('keypress',function(e){
-//     if(e.key === 'Enter'){
-//         processSearch(10);
-//     }
-// })
-//for spinner
 const taggleSpinner = loader =>{
     const loaderSection = document.getElementById('loader-section');
     if(loader){
-        loaderSection.classList.remove('hidden');
+        loaderSection.classList.remove('d-none');
     }
     else{
-        loaderSection.classList.add('hidden');
+        loaderSection.classList.add('d-none');
     }
 }
 
@@ -79,7 +71,8 @@ const ShowMoreDetails = (id) =>{
 const displayDetails = (phone) =>{
     console.log(phone);
     const phoneDetailsMidalLabel = document.getElementById('phoneDetailsMidalLabel');
-    phoneDetailsMidalLabel.innerText = phone.name;
+   const test =  phoneDetailsMidalLabel.innerText = phone.name;
+   console.log(test);
     const modalBody = document.getElementById('modal-body');
     modalBody.innerHTML = `
       <div>
@@ -93,5 +86,5 @@ const displayDetails = (phone) =>{
         <p>ReleaseDate : ${phone.releaseDate}</p>
       </div>
     `
-   
+    // phoneDetailsMidalLabel.appendChild(modalBody);
 }
